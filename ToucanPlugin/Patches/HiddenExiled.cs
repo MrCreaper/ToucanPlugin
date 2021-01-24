217f130e6b5e945f8a2cfb8f83c83b9c97000ab4
Mr.Creaper <71771134+MrCreaper@users.noreply.github.com>
2021-01-24 18:25:52 +0200

    Added AutoUpdater
    Thanks PintTheFuck And exiled UwU

---
+++ ToucanPlugin/Patches/HiddenExiled.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+
+namespace ToucanPlugin
+{
+    [HarmonyPatch(typeof(ServerConsole), nameof(ServerConsole.ReloadServerName))]
+    internal static class HiddenExiled
+    {
+        private static void Postfix()
+        {
+            if (!ToucanPlugin.Instance.Config.HiddenExiled)
+                Exiled.API.Features.Server.Name = Exiled.API.Features.Server.Name.Replace($"<color=#00000000><size=1>Exiled {Exiled.Loader.Loader.Version.ToString().Replace(".0", "")}</size></color>", "");
+        }
+    }
+}
